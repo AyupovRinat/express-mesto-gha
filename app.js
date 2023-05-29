2b356c5e31e7f7a47de94c4202b6ed120afdfe2d
Rinat Ayupov <Ayupovrinat17@yandex.ru>
2023-05-29 17:04:22 +0500

    1 коммит

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
+
 const { PORT = 3000 } = process.env;
 const app = express();
-const router = require('./routes/user');
 const mongoose = require('mongoose');
+const router = require('./routes/user');
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(express.json());
 app.use(router);
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5d8b8592978f8bd833ca8133' // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
+
+  next();
+});
 
 app.listen(PORT);
