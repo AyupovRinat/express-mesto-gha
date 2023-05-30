666f05ae8ceeec32400aa39360b6daf01c204fb1
Rinat Ayupov <Ayupovrinat17@yandex.ru>
2023-05-30 14:32:51 +0500

    3 итерация

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes/index');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
