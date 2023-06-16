9ecf6771df1709a6f77975eab589b610c929204f
Rinat Ayupov <Ayupovrinat17@yandex.ru>
2023-06-16 10:43:16 +0500

    5коммит

--- models/user.js
+++ models/user.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 const validator = require('validator');
-const UnauthorizedError = require('../errors/unauthorizedError');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -37,22 +35,5 @@ const userSchema = new mongoose.Schema({
 }, {
   versionKey: false,
 });
-
-userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email }).select('+password')
-    .then((user) => {
-      if (!user) {
-        throw new UnauthorizedError('Неправильные почта или пароль');
-      }
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            throw new UnauthorizedError('Неправильные почта или пароль');
-          }
-
-          return user;
-        });
-    });
-};
 
 module.exports = mongoose.model('user', userSchema);
