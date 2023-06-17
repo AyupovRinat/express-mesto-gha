8eb8f623ce989b76ab3b5008bdc2f2fd8ada0196
Rinat Ayupov <Ayupovrinat17@yandex.ru>
2023-06-17 19:05:50 +0500

    7 коммит

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.post('/signup', signinValidator, createUser);
 
 app.use(auth);
 
-app.use('/users', userRouter);
-app.use('/cards', cardRouter);
+app.use('/', userRouter);
+app.use('/', cardRouter);
 
 app.use('/*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
