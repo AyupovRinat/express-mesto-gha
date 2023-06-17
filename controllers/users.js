144ff1f839d0717e0fd07296e4fb4b8af2c1d175
Rinat Ayupov <Ayupovrinat17@yandex.ru>
2023-06-17 15:47:58 +0500

    6 коммит

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-const ValidationError = require('../errors/badRequestError');
 const NotFoundError = require('../errors/notFoundError');
 const ConflictError = require('../errors/conflictError');
 const BadRequestError = require('../errors/badRequestError');
@@ -14,50 +13,6 @@ module.exports.login = (req, res, next) => {
       res.send({ token });
     })
     .catch(next);
-};
-
-module.exports.getUsers = (req, res, next) => {
-  User.find({})
-    .then((data) => res.send({ data }))
-    .catch(next);
-};
-
-module.exports.getCurrentUser = (req, res, next) => {
-  User.findById(req.user._id)
-    .orFail(() => {
-      throw new Error('Notfound');
-    })
-    .then((user) => {
-      res.status(200).res.send(user);
-    })
-    .catch((err) => {
-      if (err.message === 'Notfound') {
-        next(new NotFoundError('Пользователь не найден'));
-      } else if (err.name === 'CastError' || err.name === 'ValidationError') {
-        next(new BadRequestError('Переданы некорректные данные'));
-      } else {
-        next(err);
-      }
-    });
-};
-
-module.exports.getIdUsers = (req, res, next) => {
-  User.findById(req.params.userId)
-    .orFail(() => {
-      throw new Error('Notfound');
-    })
-    .then((user) => {
-      res.status(200).res.send(user);
-    })
-    .catch((err) => {
-      if (err.message === 'Notfound') {
-        next(new NotFoundError('Пользователь не найден'));
-      } else if (err.name === 'CastError' || err.name === 'ValidationError') {
-        next(new BadRequestError('Переданы некорректные данные'));
-      } else {
-        next(err);
-      }
-    });
 };
 
 module.exports.createUser = (req, res, next) => {
@@ -85,6 +40,45 @@ module.exports.createUser = (req, res, next) => {
       } else {
         next(err);
       }
+    });
+};
+
+module.exports.getUsers = (req, res, next) => {
+  User.find({})
+    .then((data) => res.status(200).res.send({ data }))
+    .catch(next);
+};
+
+module.exports.getCurrentUser = (req, res, next) => {
+  User.findById(req.user._id)
+    .then((user) => {
+      if (user) {
+        res.status(200).send({ data: user });
+      } else {
+        next(new NotFoundError('Пользователь не найден'));
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return next(new BadRequestError('Введены некорректные данные поиска'));
+      }
+      return next(err);
+    });
+};
+
+module.exports.getIdUsers = (req, res, next) => {
+  User.findById(req.params.userId)
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Пользователь не найден');
+      }
+      res.status(200).send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return next(new BadRequestError('Введены некорректные данные поиска'));
+      }
+      return next(err);
     });
 };
 
